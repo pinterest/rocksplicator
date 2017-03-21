92435bc391fede4265c502c9eae1cba1eed8f221
Ang Xu <angxu@pinterest.com>
2017-03-20 22:22:56 -0700

    Refactor TaskBase to support type-safe construction

--- controller/controller-http/src/main/java/com/pinterest/rocksplicator/controller/resource/Tasks.java
+++ controller/controller-http/src/main/java/com/pinterest/rocksplicator/controller/resource/Tasks.java
@@ -53,7 +53,7 @@ public class Tasks {
   @GET
   @Path("/{id : [0-9]+}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Task get(@PathParam("id") Long id) {
+  public TaskInternal get(@PathParam("id") Long id) {
     return taskQueue.findTask(id);
   }
 
@@ -67,8 +67,8 @@ public class Tasks {
    */
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public List<Task> findTasks(@QueryParam("clusterName") Optional<String> clusterName,
-                              @QueryParam("state") Optional<TaskState> state) {
+  public List<TaskInternal> findTasks(@QueryParam("clusterName") Optional<String> clusterName,
+                                      @QueryParam("state") Optional<TaskState> state) {
     if (!clusterName.isPresent() && !state.isPresent()) {
       throw new WebApplicationException("Either clusterName or state must be present.",
           HttpStatus.BAD_REQUEST_400);
