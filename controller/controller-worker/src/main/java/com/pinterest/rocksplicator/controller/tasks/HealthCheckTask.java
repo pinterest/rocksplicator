ada88cc03a5c5f7fd1b9cfc5dcf45a3acda01b87
Ang Xu <angxu@pinterest.com>
2017-03-27 14:52:58 -0700

    Implement healthcheck task

---
+++ controller/controller-worker/src/main/java/com/pinterest/rocksplicator/controller/tasks/HealthCheckTask.java
@@ -0,0 +1,166 @@
+/*
+ *  Copyright 2017 Pinterest, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.pinterest.rocksplicator.controller.tasks;
+
+import com.pinterest.rocksplicator.controller.bean.ClusterBean;
+import com.pinterest.rocksplicator.controller.bean.HostBean;
+import com.pinterest.rocksplicator.controller.bean.SegmentBean;
+import com.pinterest.rocksplicator.controller.bean.ShardBean;
+import com.pinterest.rocksplicator.controller.config.ConfigParser;
+import com.pinterest.rocksplicator.controller.util.AdminClientFactory;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessLock;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.inject.Inject;
+
+
+/**
+ * @author Ang Xu (angxu@pinterest.com)
+ */
+public class HealthCheckTask extends TaskBase<HealthCheckTask.Param> {
+
+  public static final Logger LOG = LoggerFactory.getLogger(HealthCheckTask.class);
+
+  @Inject
+  public CuratorFramework zkClient;
+
+  @Inject
+  public AdminClientFactory clientFactory;
+
+  public HealthCheckTask(Param param) {
+    super(param);
+  }
+
+  @Override
+  public void process(Context ctx) throws Exception {
+    final String clusterName = getParameter().getClusterName();
+    final String zkPath = getParameter().getZkPath();
+    final InterProcessLock lock =
+        new InterProcessMutex(zkClient,  zkPath + clusterName + ".lock");
+    lock.acquire();
+    try {
+      byte[] data = zkClient.getData().forPath(zkPath + clusterName);
+      ClusterBean clusterBean = ConfigParser.parseClusterConfig(clusterName, data);
+
+      Set<InetSocketAddress> hosts = new HashSet<>();
+      for (SegmentBean segmentBean : clusterBean.getSegments()) {
+        for (HostBean hostBean : segmentBean.getHosts()) {
+          hosts.add(new InetSocketAddress(hostBean.getIp(), hostBean.getPort()));
+        }
+        // check segment replica
+        checkSegment(segmentBean, getParameter().getNumReplicas());
+      }
+
+      // ping all hosts
+      for (InetSocketAddress hostAddr : hosts) {
+        clientFactory.getClient(hostAddr).ping();
+      }
+
+      LOG.info("All hosts are good");
+
+      ctx.getTaskQueue().finishTask(ctx.getId(),
+          String.format("Cluster %s is healthy", clusterName));
+    } catch (Exception ex) {
+      ctx.getTaskQueue().failTask(ctx.getId(),
+          String.format("Cluster %s is unhealthy, reason = %s", clusterName, ex.getMessage()));
+    } finally {
+      lock.release();
+    }
+  }
+
+
+  /**
+   * Count the number of replicas for each shard within a segment.
+   * Throws exception is the number doesn't match the expected one.
+   */
+  private void checkSegment(SegmentBean segment, int numReplicas) throws Exception {
+    Map<Integer, Integer> shardCount = new HashMap<>();
+
+    for (HostBean hostBean : segment.getHosts()) {
+      for (ShardBean shardBean : hostBean.getShards()) {
+        int cnt = shardCount.getOrDefault(shardBean.getId(), 0);
+        shardCount.put(shardBean.getId(), cnt + 1);
+      }
+    }
+
+    if (shardCount.size() != segment.getNumShards()) {
+      throw new Exception(
+          String.format("Incorrect number of shards. Expected %d but actually %d.",
+              segment.getNumShards(), shardCount.size()));
+    }
+
+    Map<Integer, Integer> badShards = new HashMap<>();
+    for (Map.Entry<Integer, Integer> entry : shardCount.entrySet()) {
+      if (entry.getValue() != numReplicas) {
+        badShards.put(entry.getKey(), entry.getValue());
+      }
+    }
+
+    if (!badShards.isEmpty()) {
+      throw new Exception("Incorrect number of replicas. Bad shards: " + badShards.toString());
+    }
+  }
+
+  public static class Param extends Parameter {
+
+    @JsonProperty
+    private String clusterName;
+
+    @JsonProperty
+    private String zkPath;
+
+    @JsonProperty
+    private int numReplicas;
+
+    public String getClusterName() {
+      return clusterName;
+    }
+
+    public Param setClusterName(String clusterName) {
+      this.clusterName = clusterName;
+      return this;
+    }
+
+    public String getZkPath() {
+      return zkPath;
+    }
+
+    public Param setZkPath(String zkPath) {
+      this.zkPath = zkPath;
+      return this;
+    }
+
+    public int getNumReplicas() {
+      return numReplicas;
+    }
+
+    public Param setNumReplicas(int numReplicas) {
+      this.numReplicas = numReplicas;
+      return this;
+    }
+  }
+}
---
+++ controller/controller-worker/src/test/java/com/pinterest/rocksplicator/controller/tasks/HealthCheckTaskTest.java
@@ -0,0 +1,128 @@
+/*
+ *  Copyright 2017 Pinterest, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.pinterest.rocksplicator.controller.tasks;
+
+import com.pinterest.rocksdb_admin.thrift.Admin;
+import com.pinterest.rocksplicator.controller.FIFOTaskQueue;
+import com.pinterest.rocksplicator.controller.util.AdminClientFactory;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.apache.curator.test.TestingServer;
+import org.apache.thrift.TException;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+
+import static org.mockito.Mockito.*;
+/**
+ * @author Ang Xu (angxu@pinterest.com)
+ */
+public class HealthCheckTaskTest {
+
+  private static final String ZK_PATH = "/config/services/rocksdb/";
+  private static final String CLUSTER = "devtest";
+  private static final String CONFIG =
+      "{" +
+      "  \"user_pins\": {" +
+      "  \"num_shards\": 3," +
+      "  \"127.0.0.1:8090:us-east-1a\": [\"00000:M\", \"00001:S\", \"00002:S\"]," +
+      "  \"127.0.0.1:8091:us-east-1c\": [\"00000:S\", \"00001:M\", \"00002:S\"]," +
+      "  \"127.0.0.1:8092:us-east-1e\": [\"00000:S\", \"00001:S\", \"00002:M\"]" +
+      "   }" +
+      "}";
+
+  private TestingServer zkServer;
+  private CuratorFramework zkClient;
+
+  @Mock private Admin.Iface server;
+  @Mock private AdminClientFactory clientFactory;
+
+
+  @BeforeMethod
+  public void setup() throws Exception {
+
+    MockitoAnnotations.initMocks(this);
+
+    zkServer = new TestingServer();
+    zkClient = CuratorFrameworkFactory.newClient(
+        zkServer.getConnectString(), new RetryOneTime(3000));
+    zkClient.start();
+
+    zkClient.createContainers(ZK_PATH + CLUSTER);
+    zkClient.setData().forPath(ZK_PATH + CLUSTER, CONFIG.getBytes());
+  }
+
+  @AfterMethod
+  public void tearDown() throws IOException {
+    zkClient.close();
+    zkServer.close();
+  }
+
+  @Test
+  public void testSuccessful() throws Exception {
+    when(clientFactory.getClient(any())).thenReturn(server);
+
+    HealthCheckTask t = new HealthCheckTask(
+        new HealthCheckTask.Param()
+            .setClusterName(CLUSTER)
+            .setZkPath(ZK_PATH)
+            .setNumReplicas(3)
+    );
+    t.zkClient = zkClient;
+    t.clientFactory = clientFactory;
+
+    FIFOTaskQueue taskQueue = new FIFOTaskQueue(10);
+    Context ctx = new Context(123, CLUSTER, taskQueue, null);
+    t.process(ctx);
+
+    Assert.assertEquals(taskQueue.getResult(123), "Cluster devtest is healthy");
+  }
+
+  @Test
+  public void testFail() throws Exception {
+    final String errorMsg = "Oops..";
+    doThrow(new TException(errorMsg)).when(server).ping();
+    Mockito.when(clientFactory.getClient(Matchers.any())).thenReturn(server);
+
+    HealthCheckTask t = new HealthCheckTask(
+        new HealthCheckTask.Param()
+            .setClusterName(CLUSTER)
+            .setZkPath(ZK_PATH)
+            .setNumReplicas(3)
+    );
+    t.zkClient = zkClient;
+    t.clientFactory = clientFactory;
+
+    FIFOTaskQueue taskQueue = new FIFOTaskQueue(10);
+    Context ctx = new Context(123, CLUSTER, taskQueue, null);
+    t.process(ctx);
+
+    Assert.assertTrue(taskQueue.getResult(123).startsWith("Cluster devtest is unhealthy"));
+    Assert.assertTrue(taskQueue.getResult(123).contains(errorMsg));
+  }
+
+}
