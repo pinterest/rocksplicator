6c162fd8cdf3aa3ac9f2cb3eb16bf19be5a6d091
ywang282 <ywang282@illinois.edu>
2017-09-08 09:52:50 -0700

    make board namespace aware (#129)
    * make board namespace aware
    
    * remove extra lines

---
+++ controller/controller-board/src/app/controllers/MainController.js
@@ -0,0 +1,51 @@
+(function(){
+    angular
+        .module('app')
+        .controller('MainController', [
+            'navService', '$mdSidenav', '$mdBottomSheet', '$q', '$state', '$mdToast',
+            MainController
+        ]);
+
+    function MainController(navService, $mdSidenav, $mdBottomSheet, $q, $state, $mdToast) {
+        var vm = this;
+        vm.menuItems = [];
+        vm.selectItem = selectItem;
+        vm.toggleItemsList = toggleItemsList;
+        vm.showSimpleToast = showSimpleToast;
+        vm.toggleRightSidebar = toggleRightSidebar;
+
+        navService
+            .loadAllItems()
+            .then(function(menuItems) {
+                vm.menuItems = [].concat(menuItems);
+            });
+
+        function toggleRightSidebar() {
+            $mdSidenav('right').toggle();
+        }
+
+        function toggleItemsList() {
+            var pending = $mdBottomSheet.hide() || $q.when(true);
+
+            pending.then(function(){
+                $mdSidenav('left').toggle();
+            });
+    }
+
+    function selectItem (item) {
+        vm.toggleItemsList();
+        vm.showSimpleToast(item.name);
+        $state.reload();
+    }
+
+    function showSimpleToast(title) {
+        $mdToast.show(
+            $mdToast.simple()
+                .content(title)
+                .hideDelay(2000)
+                .position('bottom right')
+        );
+    }
+  }
+
+})();
---
+++ controller/controller-board/src/app/controllers/ShardController.js
@@ -0,0 +1,23 @@
+(function(){
+
+    angular
+        .module('app')
+        .controller('ShardController', [
+            'clusterConfigService',
+            ShardController
+        ]);
+
+    function ShardController(clusterConfigService) {
+        var vm = this;
+        vm.loadComplete = false;
+        vm.clusterSelected = clusterConfigService.getClusterName();
+        vm.dataSegments = clusterConfigService.getDataSegments();
+        vm.hostsInConfig = clusterConfigService.getHostInConfig();
+        vm.hostsNotInConfig = clusterConfigService.getHostNotInConfig();
+        vm.loadComplete = true;
+    }
+
+})();
+
+
+
