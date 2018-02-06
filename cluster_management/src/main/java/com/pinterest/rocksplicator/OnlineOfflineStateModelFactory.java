59b03b773d0e6215f169165227446abbfe834fa7
Bo Liu <bol@pinterest.com>
2018-02-06 13:52:15 -0800

    [cluster_management] helix participant for readonly clusters

---
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/OnlineOfflineStateModelFactory.java
@@ -0,0 +1,185 @@
+package com.pinterest.rocksplicator;
+
+import com.pinterest.rocksdb_admin.thrift.AddDBRequest;
+import com.pinterest.rocksdb_admin.thrift.AddS3SstFilesToDBRequest;
+import com.pinterest.rocksdb_admin.thrift.Admin;
+import com.pinterest.rocksdb_admin.thrift.AdminException;
+import com.pinterest.rocksdb_admin.thrift.ClearDBRequest;
+import com.pinterest.rocksdb_admin.thrift.CloseDBRequest;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.helix.NotificationContext;
+import org.apache.helix.model.Message;
+import org.apache.helix.participant.statemachine.StateModel;
+import org.apache.helix.participant.statemachine.StateModelFactory;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OnlineOfflineStateModelFactory extends StateModelFactory<StateModel> {
+  private final int adminPort;
+  private final String cluster;
+  private CuratorFramework zkClient;
+
+  private static final Logger LOG = LoggerFactory.getLogger(OnlineOfflineStateModelFactory.class);
+
+  OnlineOfflineStateModelFactory(int adminPort, String zkConnectString, String cluster) {
+    this.adminPort = adminPort;
+    this.cluster = cluster;
+    this.zkClient = CuratorFrameworkFactory.newClient(zkConnectString,
+        new ExponentialBackoffRetry(1000, 3));
+    zkClient.start();
+  }
+
+  @Override
+  public StateModel createNewStateModel(String resourceName, String partitionName) {
+    LOG.info("Create a new state for " + partitionName);
+    return new OnlineOfflineStateModel(resourceName, partitionName, adminPort, cluster, zkClient);
+  }
+
+  public static class OnlineOfflineStateModel extends StateModel {
+    private static final Logger LOG = LoggerFactory.getLogger(OnlineOfflineStateModel.class);
+
+    private final String resourceName;
+    private final String partitionName;
+    private final int adminPort;
+    private final String cluster;
+    private CuratorFramework zkClient;
+
+    /**
+     * State model that handles the state machine of a single partition.
+     */
+    public OnlineOfflineStateModel(String resourceName, String partitionName, int adminPort,
+                                   String cluster, CuratorFramework zkClient) {
+      this.resourceName = resourceName;
+      this.partitionName = partitionName;
+      this.adminPort = adminPort;
+      this.cluster = cluster;
+      this.zkClient = zkClient;
+    }
+
+    /**
+     * Callback for OFFLINE to ONLINE transition.
+     * This can happen when 1) a new partition is first launched on a host; or 2) an existing
+     * partition is reopened when restart/zk temporarily expires.
+     * We first make sure the DB is open and then load SST files into the DB.
+     */
+    public void onBecomeOnlineFromOffline(Message message, NotificationContext context) {
+      checkSanity("OFFLINE", "ONLINE", message);
+      LOG.info("Switching from " + message.getFromState() + " to " + message.getToState()
+          + " for " + message.getPartitionName());
+
+      Admin.Client client;
+      try {
+        client = getLocalAdminClient();
+      } catch (TTransportException e) {
+        LOG.error("Failed to connect to local Admin port", e);
+        throw new RuntimeException(e);
+      }
+
+      try {
+        AddDBRequest req = new AddDBRequest(getDbName(), "localhost");
+        client.addDB(req);
+      } catch (Exception e) {
+        // adding db is best-effort
+        LOG.info("Failed to add " + partitionName, e);
+      }
+
+      try {
+        zkClient.sync().forPath(getS3BucketLocation());
+        String s3Bucket = new String(zkClient.getData().forPath(getS3BucketLocation()));
+        zkClient.sync().forPath(getS3PathLocation());
+        String s3Path = new String(zkClient.getData().forPath(getS3PathLocation()));
+
+        AddS3SstFilesToDBRequest req = new AddS3SstFilesToDBRequest(getDbName(),
+            s3Bucket, s3Path);
+        client.addS3SstFilesToDB(req);
+      } catch (Exception e) {
+        LOG.error("Failed to add S3 files for " + partitionName, e);
+        throw new RuntimeException(e);
+      }
+    }
+
+    /**
+     * Callback for ONLINE to OFFLINE transition.
+     * The callback simply close the DB.
+     */
+    public void onBecomeOfflineFromOnline(Message message, NotificationContext context) {
+      checkSanity("ONLINE", "OFFLINE", message);
+      LOG.info("Switching from " + message.getFromState() + " to " + message.getToState()
+          + " for " + message.getPartitionName());
+
+      try {
+        Admin.Client client = getLocalAdminClient();
+        CloseDBRequest req = new CloseDBRequest(getDbName());
+        client.closeDB(req);
+      } catch (AdminException e) {
+        LOG.info(partitionName + " doesn't exist", e);
+      } catch (TTransportException e) {
+        LOG.error("Failed to connect to local Admin port", e);
+      } catch (TException e) {
+        LOG.error("CloseDB() request failed", e);
+      }
+    }
+
+    /**
+     * Callback for OFFLINE to DROPPED transition.
+     * The callback simply clear the DB.
+     */
+    public void onBecomeDroppedFromOffline(Message message, NotificationContext context) {
+      checkSanity("OFFLINE", "DROPPED", message);
+      LOG.info("Switching from " + message.getFromState() + " to " + message.getToState()
+          + " for " + message.getPartitionName());
+
+      try {
+        Admin.Client client = getLocalAdminClient();
+        ClearDBRequest req = new ClearDBRequest(getDbName());
+        req.setReopen_db(false);
+        client.clearDB(req);
+      } catch (AdminException e) {
+        LOG.error("Failed to destroy DB", e);
+      } catch (TTransportException e) {
+        LOG.error("Failed to connect to local Admin port", e);
+      } catch (TException e) {
+        LOG.error("ClearDB() request failed", e);
+      }
+    }
+
+    private Admin.Client getLocalAdminClient() throws TTransportException {
+      TSocket sock = new TSocket("localhost", adminPort);
+      sock.open();
+      return new Admin.Client(new TBinaryProtocol(sock));
+    }
+
+    private void checkSanity(String fromState, String toState, Message message) {
+      if (fromState.equalsIgnoreCase(message.getFromState())
+          && toState.equalsIgnoreCase(message.getToState())
+          && resourceName.equalsIgnoreCase(message.getResourceName())
+          && partitionName.equalsIgnoreCase(message.getPartitionName())) {
+        return;
+      }
+
+      LOG.error("Invalid meesage: " + message.toString());
+      LOG.error("From " + fromState + " to " + toState + " for " + partitionName);
+    }
+
+    private String getS3BucketLocation() {
+      return "/metadata/" + this.cluster + "/" + this.resourceName + "/s3_bucket";
+    }
+
+    private String getS3PathLocation() {
+      return "/metadata/" + this.cluster + "/" + this.resourceName + "/s3_path";
+    }
+
+    // partition name is in format: test_0
+    // DB name is in format: test00000
+    private String getDbName() {
+      String[] parts = partitionName.split("_");
+      return String.format("%s%5d", parts[0], parts[1]);
+    }
+  }
+}
---
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/Participant.java
@@ -0,0 +1,135 @@
+package com.pinterest.rocksplicator;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.helix.HelixAdmin;
+import org.apache.helix.HelixManager;
+import org.apache.helix.HelixManagerFactory;
+import org.apache.helix.InstanceType;
+import org.apache.helix.manager.zk.HelixManagerShutdownHook;
+import org.apache.helix.manager.zk.ZKHelixAdmin;
+import org.apache.helix.model.HelixConfigScope;
+import org.apache.helix.model.Message;
+import org.apache.helix.model.builder.HelixConfigScopeBuilder;
+import org.apache.helix.participant.StateMachineEngine;
+import org.apache.helix.participant.statemachine.StateModel;
+import org.apache.helix.participant.statemachine.StateModelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Participant {
+  private static final Logger LOG = LoggerFactory.getLogger(Participant.class);
+  private static final String zkServer = "zkSvr";
+  private static final String cluster = "cluster";
+  private static final String domain = "domain";
+  private static final String hostAddress = "host";
+  private static final String hostPort = "port";
+  private static final String stateModel = "stateModelType";
+
+  private HelixManager helixManager;
+  private StateModelFactory<StateModel> stateModelFactory;
+
+  private static Options constructCommandLineOptions() {
+    Option zkServerOption =
+        OptionBuilder.withLongOpt(zkServer).withDescription("Provide zookeeper addresses").create();
+    zkServerOption.setArgs(1);
+    zkServerOption.setRequired(true);
+    zkServerOption.setArgName("ZookeeperServerAddresses(Required)");
+
+    Option clusterOption =
+        OptionBuilder.withLongOpt(cluster).withDescription("Provide cluster name").create();
+    clusterOption.setArgs(1);
+    clusterOption.setRequired(true);
+    clusterOption.setArgName("Cluster name (Required)");
+
+    Option domainOption =
+        OptionBuilder.withLongOpt(domain).withDescription("Provide instance domain").create();
+    domainOption.setArgs(1);
+    domainOption.setRequired(true);
+    domainOption.setArgName("Instance domain (Required)");
+
+    Option hostOption =
+        OptionBuilder.withLongOpt(hostAddress).withDescription("Provide host name").create();
+    hostOption.setArgs(1);
+    hostOption.setRequired(true);
+    hostOption.setArgName("Host name (Required)");
+
+    Option portOption =
+        OptionBuilder.withLongOpt(hostPort).withDescription("Provide host port").create();
+    portOption.setArgs(1);
+    portOption.setRequired(true);
+    portOption.setArgName("Host port (Required)");
+
+    Option stateModelOption =
+        OptionBuilder.withLongOpt(stateModel).withDescription("StateModel Type").create();
+    stateModelOption.setArgs(1);
+    stateModelOption.setRequired(true);
+    stateModelOption.setArgName("StateModel Type (Required)");
+
+    Options options = new Options();
+    options.addOption(zkServerOption)
+        .addOption(clusterOption)
+        .addOption(domainOption)
+        .addOption(hostOption)
+        .addOption(portOption)
+        .addOption(stateModelOption);
+    return options;
+  }
+
+  private static CommandLine processCommandLineArgs(String[] cliArgs) throws ParseException {
+    CommandLineParser cliParser = new GnuParser();
+    Options cliOptions = constructCommandLineOptions();
+    return cliParser.parse(cliOptions, cliArgs);
+  }
+
+  /**
+   * Start a Helix participant.
+   * @param args command line parameters
+   */
+  public static void main(String[] args) throws Exception {
+    CommandLine cmd = processCommandLineArgs(args);
+    final String zkConnectString = cmd.getOptionValue(zkServer);
+    final String clusterName = cmd.getOptionValue(cluster);
+    final String domainName = cmd.getOptionValue(domain);
+    final String host = cmd.getOptionValue(hostAddress);
+    final String port = cmd.getOptionValue(hostPort);
+    final String stateModelType = cmd.getOptionValue(stateModel);
+    final String instanceName = host + "_" + port;
+
+    LOG.info("Starting participant with ZK:" + zkConnectString);
+    Participant participant = new Participant(zkConnectString, clusterName, instanceName,
+        stateModelType, Integer.parseInt(port));
+
+    HelixAdmin helixAdmin = new ZKHelixAdmin(zkConnectString);
+    HelixConfigScope scope =
+        new HelixConfigScopeBuilder(HelixConfigScope.ConfigScopeProperty.PARTICIPANT)
+            .forCluster(clusterName).forParticipant(instanceName)
+            .build();
+    Map<String, String> properties = new HashMap<String, String>();
+    properties.put("DOMAIN", domainName + ",instance=" + instanceName);
+    helixAdmin.setConfig(scope, properties);
+
+    Thread.currentThread().join();
+  }
+
+  public Participant(String zkConnectString, String clusterName, String instanceName,
+                     String stateModelType, int port) throws Exception {
+    helixManager = HelixManagerFactory.getZKHelixManager(clusterName, instanceName,
+        InstanceType.PARTICIPANT, zkConnectString);
+    stateModelFactory = new OnlineOfflineStateModelFactory(port, zkConnectString, clusterName);
+    StateMachineEngine stateMach = helixManager.getStateMachineEngine();
+    stateMach.registerStateModelFactory(stateModelType, stateModelFactory);
+    helixManager.connect();
+    helixManager.getMessagingService().registerMessageHandlerFactory(
+        Message.MessageType.STATE_TRANSITION.name(), stateMach);
+    Runtime.getRuntime().addShutdownHook(new HelixManagerShutdownHook(helixManager));
+  }
+}
