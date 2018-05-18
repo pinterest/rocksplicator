4ef21393c1b976e8d71d525abbae0dec0580c855
Bo Liu <bol@pinterest.com>
2018-05-17 17:04:39 -0700

    [counter] adopt helix for the counter example

--- examples/counter_service/counter.cpp
+++ examples/counter_service/counter.cpp
@@ -32,12 +32,22 @@
 #include "common/stats/stats.h"
 #include "common/stats/status_server.h"
 #include "gflags/gflags.h"
+#include "rocksdb_admin/helix_client.h"
 #include "thrift/lib/cpp2/server/ThriftServer.h"
 
 
 DEFINE_int32(num_worker_threads, 256, "Total number of thrift worker threads");
 DEFINE_int32(num_server_io_threads, sysconf(_SC_NPROCESSORS_ONLN),
              "The number of threads for processing server IO");
+
+DEFINE_string(helix_zk_connect_str, "127.0.0.1:2181",
+              "ZK cluster connect string");
+
+DEFINE_string(helix_cluster_name, "", "Which helix cluster to join");
+
+DEFINE_bool(read_only_mode, true, "If the cluster does batch uploading only");
+
+DEFINE_string(post_url, "", "The url to post the shard mapping file to");
 
 DECLARE_string(shard_config_path);
 DECLARE_int32(port);
@@ -53,8 +63,14 @@ int main(int argc, char** argv) {
     common::getAvailabilityZone(), FLAGS_shard_config_path);
   auto server = std::make_unique<apache::thrift::ThriftServer>();
 
+  const bool helix_mode = !FLAGS_helix_cluster_name.empty();
+  std::unique_ptr<admin::ApplicationDBManager> db_manager;
+  if (helix_mode) {
+      db_manager = std::make_unique<admin::ApplicationDBManager>();
+  }
+
   auto handler = std::make_unique<counter::CounterHandler>(
-    nullptr, counter::GetRocksdbOptions,
+    std::move(db_manager), counter::GetRocksdbOptions,
     std::move(router), rocksdb::WriteOptions(), rocksdb::ReadOptions());
 
   server->setInterface(std::move(handler));
@@ -66,6 +82,22 @@ int main(int argc, char** argv) {
   server->setNWorkerThreads(FLAGS_num_server_io_threads);
 
   common::StatusServer::StartStatusServer();
+
+  if (helix_mode) {
+      auto pg = "pg"; // put your own placement query function here if you want
+      auto az = common::getAvailabilityZone();
+      CHECK(!az.empty()) << "Failed to get az";
+      // e.g., "az=us-east-1a,pg=placement_group1"
+      auto domain = "az=" + az + ",pg=" + (pg.empty() ? az : pg);
+      LOG(INFO) << "Joining Helix cluster with domain " << domain;
+      admin::JoinCluster(FLAGS_helix_zk_connect_str,
+                         FLAGS_helix_cluster_name,
+                         FLAGS_read_only_mode ? "OnlineOffline" : "MasterSlave",
+                         domain,
+                         "/mnt/counter/bin/",
+                         FLAGS_post_url);
+  }
+
   LOG(INFO) << "Starting server at port " << FLAGS_port;
   server->serve();
   
