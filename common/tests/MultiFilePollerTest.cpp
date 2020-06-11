be23e24432fbe44f6c85652e14c03d49f8bebd88
Gopal Rajpurohit <grajpurohit@pinterest.com>
2020-06-11 14:19:06 -0700

    Rocksplicator: Add FilePoller and MultiFilePoller from wangle library into Rocksplicator

--- common/tests/MultiFilePollerTest.cpp
+++ common/tests/MultiFilePollerTest.cpp
@@ -24,7 +24,6 @@
 #include <folly/String.h>
 #include <folly/experimental/TestUtil.h>
 #include <folly/portability/GTest.h>
-#include <folly/synchronization/SaturatingSemaphore.h>
 
 using namespace common;
 using namespace folly::test;
@@ -33,7 +32,7 @@ using namespace folly::test;
 static const std::chrono::milliseconds kPollIntervalMs{200};
 
 // FilePoller's min granularity.
-static const std::chrono::milliseconds kWriteWaitMs{1000};
+static const std::chrono::milliseconds kWriteWaitMs{500};
 
 // Should be a few times larger than kPollInterval.
 static const std::chrono::milliseconds kMaxSemaphoreWaitMs{1000};
@@ -73,6 +72,9 @@ TEST_F(MultiFilePollerTest, BasicTest) {
   const std::string f(tmpdirPath_ + "/Basic1"), d1("a"), d2("b"), d3("c");
   size_t count = 0;
 
+  std::promise<bool> promise;
+  std::promise<bool> promiseAfterCancellation;
+
   // Write initial data.
   ASSERT_TRUE(folly::writeFile(d1, f.c_str()));
 
@@ -82,14 +84,17 @@ TEST_F(MultiFilePollerTest, BasicTest) {
         auto& content = folly::get_or_throw(newData, f);
         EXPECT_EQ(d2, content);
         EXPECT_EQ(1, ++count);
+        if (!promise.get_future().valid()) {
+          promise.set_value(true);
+        } else if (!promiseAfterCancellation.get_future().valid()) {
+          promiseAfterCancellation.set_value(false);
+        }
       });
 
   delayedWrite(f, d2);
 
   // Check whether the callback is triggered by acquiring the semaphore.
-  ASSERT_TRUE(sem.try_wait_until(std::chrono::steady_clock::now() +
-                                 kMaxSemaphoreWaitMs));
-  sem.reset();
+  ASSERT_TRUE(promise.get_future().get());
   ASSERT_EQ(1, count);
 
   // Cancel the callback.
@@ -98,8 +103,9 @@ TEST_F(MultiFilePollerTest, BasicTest) {
   // Write to the file again. The callback should not run.
   delayedWrite(f, d3);
 
-  ASSERT_FALSE(sem.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
+  ASSERT_TRUE(promiseAfterCancellation.get_future().wait_for(
+    kMaxSemaphoreWaitMs) == std::future_status::timeout);
+
   // If the callback runs, the assertion inside callback will also fail.
 }
 
@@ -138,10 +144,21 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
   const std::string f2(tmpdirPath_ + "/Complex2"), d2("b"), d21("B"), d22("X");
   const std::string f3(tmpdirPath_ + "/Complex3"), d3("c");
 
-  folly::SaturatingSemaphore<true> sem1, sem2, sem3, sem4;
   std::string data2, data4;
   std::vector<std::string> data3;
   size_t count1 = 0, count2 = 0, count3 = 0, count4 = 0;
+
+  std::promise<bool> promise1;
+  std::promise<bool> promise1_cancelled;
+  std::promise<bool> promise21;
+  std::promise<bool> promise22;
+  std::promise<bool> promise22_cancelled;
+  std::promise<bool> promise31;
+  std::promise<bool> promise32;
+  std::promise<bool> promise33;
+  std::promise<bool> promise41;
+  std::promise<bool> promise42;
+  std::promise<bool> promise4_cancelled;
 
   // cb1 is only triggered once. It expects the content to equal d1, which is
   // written to the file to trigger the callback.
@@ -150,15 +167,24 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
         auto& content = folly::get_or_throw(newData, f1);
         EXPECT_EQ(d1, content);
         EXPECT_EQ(1, ++count1); // Fail if run more than once.
-        sem1.post();
-      });
+      promise1.set_value(true);
+      if (!promise1.get_future().valid()) {
+        promise1.set_value(true);
+      } else if (!promise1_cancelled.get_future().valid()) {
+        promise1_cancelled.set_value(false);
+      }
+    });
 
   // cb2 copies content of f2 from arg to data2, and increments count2.
   auto cb2 = updater_->registerFile(
       f2, [&](const MultiFilePoller::CallbackArg& newData) {
         data2 = folly::get_or_throw(newData, f2);
         count2++;
-        sem2.post();
+        if (!promise21.get_future().valid()) {
+          promise21.set_value(true);
+        } else if (!promise22.get_future().valid()) {
+          promise22.set_value(true);
+        }
       });
 
   // cb3 concatenates the data of f3 and f3 and writes the value to data3.
@@ -168,22 +194,26 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
           // When we write to f3 to trigger cb3, f1 does not exist yet.
           // Check that f1 is not in the map.
           ASSERT_EQ(newData.end(), newData.find(f1));
+          promise31.set_value(true);
         } else {
           // Otherwise f1 must exist in the map.
           ASSERT_NE(newData.end(), newData.find(f1));
+          if (!promise32.get_future().valid()) {
+            promise32.set_value(true);
+          } else if (!promise33.get_future().valid()) {
+            promise33.set_value(true);
+          }
         }
         const auto& f3Data = folly::get_or_throw(newData, f3);
         const auto& f1Data = folly::get_default(newData, f1, "<NODATA>");
         data3 = {f3Data, f1Data};
         count3++;
-        sem3.post();
       });
 
   // Create f2 to trigger cb2.
   ASSERT_TRUE(folly::writeFile(d2, f2.c_str()));
-  ASSERT_TRUE(sem2.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem2.reset();
+  ASSERT_TRUE(promise2.get_future().get());
+
   EXPECT_EQ(1, count2); // +1.
   EXPECT_EQ(0, count1); // No change.
   EXPECT_EQ(0, count3); // No change.
@@ -191,9 +221,7 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
 
   // Create f3 to trigger cb3. Note that cb1 should not run.
   ASSERT_TRUE(folly::writeFile(d3, f3.c_str()));
-  ASSERT_TRUE(sem3.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem3.reset();
+  ASSERT_TRUE(promise31.get_future().get());
   EXPECT_EQ(1, count3); // +1.
   EXPECT_EQ(0, count1); // No change.
   EXPECT_EQ(1, count2); // No change.
@@ -201,14 +229,11 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
 
   // Create f1 to trigger cb3 and cb1. Order doesn't matter.
   ASSERT_TRUE(folly::writeFile(d1, f1.c_str()));
-  ASSERT_TRUE(sem1.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem1.reset();
+  ASSERT_TRUE(promise1.get_future().get());
+
   EXPECT_EQ(1, count1); // +1.
   EXPECT_EQ(1, count2); // No change.
-  ASSERT_TRUE(sem3.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem3.reset();
+  ASSERT_TRUE(promise32.get_future().get());
   EXPECT_EQ(2, count3);              // +1.
   EXPECT_EQ(std::vector<std::string>({d3, d1}), data3);
 
@@ -217,18 +242,21 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
       f2, [&](const MultiFilePoller::CallbackArg& newData) {
         data4 = folly::get_or_throw(newData, f2);
         count4++;
-        sem4.post();
+        if (!promise41.get_future().valid()) {
+          promise41.set_value(true);
+        } else if (!promise42.get_future().valid()) {
+          promise42.set_value(true);
+        } else if (!promise4_cancelled.get_future().valid()) {
+          promise4_cancelled.set_value(false);
+        }
       });
 
   // Write to f2 to trigger second callback.
   delayedWrite(f2, d21);
 
-  ASSERT_TRUE(sem2.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem2.reset();
-  ASSERT_TRUE(sem4.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem4.reset();
+  ASSERT_TRUE(promise22.get_future().get());
+  ASSERT_TRUE(promise4.get_future().get());
+
   EXPECT_EQ(2, count2);  // +1.
   EXPECT_EQ(1, count4);  // +1.
   EXPECT_EQ(1, count1);  // No change.
@@ -239,11 +267,10 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
   // f1 is in two different callbacks. Cancel cb1 should not affect cb3.
   updater_->cancelCallback(cb1);
   ASSERT_TRUE(folly::writeFile(d11, f1.c_str())); // Last write to f1 > 1s ago.
-  ASSERT_TRUE(sem3.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem3.reset();
-  ASSERT_FALSE(sem1.try_wait_until(std::chrono::steady_clock::now() +
-                                   kMaxSemaphoreWaitMs));
+  ASSERT_TRUE(promise33.get_future().get());
+  ASSERT_TRUE(promise1_cancelled.get_future().wait_for(
+    kMaxSemaphoreWaitMs) == std::future_status::timeout);
+
   EXPECT_EQ(3, count3); // +1.
   EXPECT_EQ(1, count1); // No change.
   EXPECT_EQ(2, count2); // No change.
@@ -253,11 +280,10 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
   // cb2 and cb4 use and only use f2. Cancel cb2 should not affect cb4.
   updater_->cancelCallback(cb2);
   ASSERT_TRUE(folly::writeFile(d22, f2.c_str())); // Last write to f2 > 1s ago.
-  ASSERT_TRUE(sem4.try_wait_until(std::chrono::steady_clock::now() +
-                                  kMaxSemaphoreWaitMs));
-  sem4.reset();
-  ASSERT_FALSE(sem2.try_wait_until(std::chrono::steady_clock::now() +
-                                   kMaxSemaphoreWaitMs));
+  ASSERT_TRUE(promise42.get_future().get());
+  ASSERT_TRUE(promise2_cancelled.get_future().wait_for(
+    kMaxSemaphoreWaitMs) == std::future_status::timeout);
+
   EXPECT_EQ(d21, data2); // cb2 should not run, so not updated.
   EXPECT_EQ(d22, data4); // cb4 should run, so updated to d22.
   EXPECT_EQ(2, count2);  // No change.
@@ -266,8 +292,9 @@ TEST_F(MultiFilePollerTest, ComplexTest) {
   // Now we cancel cb4. Record of f2 should be cleaned up.
   updater_->cancelCallback(cb4);
   ASSERT_TRUE(folly::writeFile(d1, f2.c_str()));
-  ASSERT_FALSE(sem4.try_wait_until(std::chrono::steady_clock::now() +
-                                   kMaxSemaphoreWaitMs));
+  ASSERT_TRUE(promise4_cancelled.get_future().wait_for(
+    kMaxSemaphoreWaitMs) == std::future_status::timeout);
+
   EXPECT_EQ(d21, data2); // cb2 should not run, so not updated to d1.
   EXPECT_EQ(d22, data4); // cb4 should not run, so not updated to d1.
 }
