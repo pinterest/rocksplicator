ee5179fa54c85ed0469c285cb897efae391e11dc
Gopal Rajpurohit <grajpurohit@pinterest.com>
2021-01-19 10:32:23 -0800

    Initial thrift structs for profiling leadership handoff to maintain events history

--- cluster_management/src/main/thrift/events_history.thrift
+++ cluster_management/src/main/thrift/events_history.thrift
@@ -16,40 +16,79 @@
 namespace java com.pinterest.rocksplicator.thrift.eventhistory
 
 enum LeaderEventType {
-  # Next new enum value = 21
-  # Next placeholder enum value to use : 13
+  # All of the event types PREFIXED with PARTICIPANT
+  # originates from PARTICIPANT which is an existing leader
+  # for a given partition or a PARTICIPANT which is trying
+  # to become a new leader.
   PARTICIPANT_LEADER_DOWN_INIT = 1,
   PARTICIPANT_LEADER_DOWN_SUCCESS = 2,
   PARTICIPANT_LEADER_DOWN_FAILURE = 3,
   PARTICIPANT_LEADER_UP_INIT = 4,
   PARTICIPANT_LEADER_UP_SUCCESS = 5,
   PARTICIPANT_LEADER_UP_FAILURE = 6,
-  SPECTATOR_OBSERVED_LEADER_DOWN = 7,
-  SPECTATOR_OBSERVED_LEADER_UP   = 8,
-  SPECTATOR_POSTED_SHARDMAP_LEADER_UP = 9,
-  SPECTATOR_POSTED_SHARDMAP_LEADER_DOWN = 10,
-  CLIENT_OBSERVED_SHARDMAP_LEADER_UP = 11,
-  CLIENT_OBSERVED_SHARDMAP_LEADER_DOWN = 12,
-
   # Following events are currently not used
   # Placeholder events have been placed to
   # help with future migration to new event types
-  PLACEHOLDER_EVENT_TYPE_13 = 13
-  PLACEHOLDER_EVENT_TYPE_14 = 14
-  PLACEHOLDER_EVENT_TYPE_15 = 15
-  PLACEHOLDER_EVENT_TYPE_16 = 16
-  PLACEHOLDER_EVENT_TYPE_17 = 17
-  PLACEHOLDER_EVENT_TYPE_18 = 18
-  PLACEHOLDER_EVENT_TYPE_19 = 19
-  PLACEHOLDER_EVENT_TYPE_20 = 20
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_07 = 7
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_08 = 8
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_09 = 9
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_10 = 10
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_11 = 11
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_12 = 12
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_13 = 13
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_14 = 14
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_15 = 15
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_16 = 16
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_17 = 17
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_18 = 18
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_19 = 19
+  PARTICIPANT_PLACEHOLDER_EVENT_TYPE_20 = 20
+
+  # All of the following events are as observed
+  # from a leader spectator instance.
+  # spectator instance only logs a specific
+  # leader being down or up for the first time
+  # it observes the state change for leader.
+  SPECTATOR_OBSERVED_LEADER_DOWN = 100,
+  SPECTATOR_OBSERVED_LEADER_UP   = 101,
+  SPECTATOR_POSTED_SHARDMAP_LEADER_UP = 101,
+  SPECTATOR_POSTED_SHARDMAP_LEADER_DOWN = 102,
+
+  # All of the following events are / will be
+  # logged from a client that watches the shard_map
+  # generated from SPECTATOR. We can use Specator
+  # to watch on shard_maps propagation delays from itself.
+  CLIENT_OBSERVED_SHARDMAP_LEADER_UP = 200,
+  CLIENT_OBSERVED_SHARDMAP_LEADER_DOWN = 201,
 }
 
+/**
+ * A particular instance of event related to leadership handoff.
+ **/
 struct LeaderEvent {
   1: optional LeaderEventType event_type,
-  2: optional string observer_node,
-  3: optional i64 timestamp_ms,
+  # Node that is logging this event
+  2: optional string originating_node,
+  # If available, which leader node was observed.
+  # participant will always be observing itself as leader
+  # while performing state transitions. Hence participant
+  # can skip populating it, as it will be same as
+  # originating node.
+  #
+  3: optional string observed_leader_node,
+  # the timestamp in milliseconds of the
+  # event as close to  when the event actually was
+  # observed for the first time, and it's effect
+  # applied. This is not the update time in external
+  # storage when the event got logged, since there can
+  # potentially be a delay when the event is observed
+  # and when it actually got logged
+  4: optional i64 event_timestamp_ms,
 }
 
+/**
+ * History of events kept sorted in event_timestamp_ms order
+ **/
 struct LeaderEventsHistory {
   1: required i32 max_events_to_keep = 25;
   2: optional list<LeaderEvent> events,
