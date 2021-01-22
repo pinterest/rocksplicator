3d69fa66dced9be16199529f770f399957270c97
Gopal Rajpurohit <grajpurohit@pinterest.com>
2021-01-22 11:48:07 -0800

    Initial Codec implementations that will be used to serialized thrift structures to zk

--- cluster_management/src/test/java/com/pinterest/rocksplicator/codecs/ThriftCodecTest.java
+++ cluster_management/src/test/java/com/pinterest/rocksplicator/codecs/ThriftCodecTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import com.pinterest.rocksplicator.thrift.eventhistory.LeaderEventsHistory;
 
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
 import org.junit.Test;
 
 public class ThriftCodecTest extends CodecTestBase {
@@ -11,8 +13,7 @@ public class ThriftCodecTest extends CodecTestBase {
   @Test
   public void testBinaryCodec() throws Exception {
     ThriftCodec<LeaderEventsHistory>
-        codec =
-        ThriftCodec.createBinaryCodec(LeaderEventsHistory.class);
+        codec = new ThriftCodec<>(LeaderEventsHistory.class, new TBinaryProtocol.Factory());
     byte[] binaryData = codec.encode(history);
     LeaderEventsHistory decodedHistory = codec.decode(binaryData);
     assertEquals(history, decodedHistory);
@@ -21,8 +22,7 @@ public class ThriftCodecTest extends CodecTestBase {
   @Test
   public void testCompactCodec() throws Exception {
     ThriftCodec<LeaderEventsHistory>
-        codec =
-        ThriftCodec.createCompactCodec(LeaderEventsHistory.class);
+        codec = new ThriftCodec<>(LeaderEventsHistory.class, new TCompactProtocol.Factory());
     byte[] binaryData = codec.encode(history);
     LeaderEventsHistory decodedHistory = codec.decode(binaryData);
     assertEquals(history, decodedHistory);
