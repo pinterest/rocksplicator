17618e8809cb98927a24da99950059841ea6b1f9
Gopal Rajpurohit <grajpurohit@pinterest.com>
2021-01-25 13:40:32 -0800

    Implementation of zkstore

--- cluster_management/src/main/java/com/pinterest/rocksplicator/eventstore/DescendingTimestampLeaderEventComparator.java
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/eventstore/DescendingTimestampLeaderEventComparator.java
@@ -1,13 +1,29 @@
+/// Copyright 2021 Pinterest Inc.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+/// http://www.apache.org/licenses/LICENSE-2.0
+
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+//
+// @author Gopal Rajpurohit (grajpurohit@pinterest.com)
+//
+
 package com.pinterest.rocksplicator.eventstore;
 
 import com.pinterest.rocksplicator.thrift.eventhistory.LeaderEvent;
 
 import java.util.Comparator;
 
-/**
- *
- */
 class DescendingTimestampLeaderEventComparator implements Comparator<LeaderEvent> {
+
   @Override
   public int compare(LeaderEvent first, LeaderEvent second) {
     return -Long
