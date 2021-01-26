b9c245877a2d731f4218e61a88229b8e139ffd4c
Kangnan Li <kangnanli@pinterest.com>
2021-01-26 19:13:35 +0000

    add fail count for configGenerator

--- cluster_management/src/main/java/com/pinterest/rocksplicator/monitoring/mbeans/RocksplicatorSpectatorMonitorMBean.java
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/monitoring/mbeans/RocksplicatorSpectatorMonitorMBean.java
@@ -3,6 +3,7 @@ package com.pinterest.rocksplicator.monitoring.mbeans;
 public interface RocksplicatorSpectatorMonitorMBean {
   // ConfigGenerator
   public long getConfigGeneratorCalledCount();
+  public long getConfigGeneratorFailCount();
   public double getMeanConfigGeneratorLatency();
   public double getMaxConfigGeneratorLatency();
   public double getMinConfigGeneratorLatency();
