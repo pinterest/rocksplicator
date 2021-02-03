975cb0e2ab4b3f9fa156ee6deff94d8c83ea0106
Gopal Rajpurohit <grajpurohit@pinterest.com>
2021-02-03 15:14:33 -0800

    Implement client events logger and driver mechanism to notify based on file / config changes and whether one is currently a leader client

--- cluster_management/src/main/java/com/pinterest/rocksplicator/eventstore/ClientShardMapLeaderEventLogger.java
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/eventstore/ClientShardMapLeaderEventLogger.java
@@ -18,25 +18,10 @@
 
 package com.pinterest.rocksplicator.eventstore;
 
+import com.pinterest.rocksplicator.shardmap.ShardMap;
+
 import java.io.Closeable;
 
-public interface LeaderEventsLogger extends Closeable {
-  /**
-   * Provides a collector of leader events for a given resource and partition.
-   * All logically batched events are collected in collector before committing it to
-   * backing store.
-   */
-  LeaderEventsCollector newEventsCollector(final String resourceName, final String partitionName);
-
-  /**
-   * True if logging is generally enabled.
-   */
-  boolean isLoggingEnabled();
-
-  /**
-   * True is logging is enabled for a given resource.
-   */
-  boolean isLoggingEnabledForResource(final String resourceName);
-
-  void resetCache();
+public interface ClientShardMapLeaderEventLogger extends Closeable {
+  void process(ShardMap shardMap, long shardMapNotificationTimeMillis);
 }
