e90a9bc3c38bf2fe9fc23bfc8c12eb2a8d879f34
Gopal Rajpurohit <grajpurohit@pinterest.com>
2021-02-04 22:47:12 -0800

    Framework implementation of distributed spectator at par with controller cluster in distributed mode

---
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/spectator/ConfigGeneratorClusterSpectatorFactory.java
@@ -0,0 +1,40 @@
+/// Copyright 2021 Pinterest Inc.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+/// http://www.apache.org/licenses/LICENSE-2.0
+
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+//
+// @author Gopal Rajpurohit (grajpurohit@pinterest.com)
+//
+
+package com.pinterest.rocksplicator.spectator;
+
+public class ConfigGeneratorClusterSpectatorFactory implements ClusterSpectatorFactory {
+
+  private final String uriPattern;
+
+  public ConfigGeneratorClusterSpectatorFactory(String uriPattern) {
+    this.uriPattern = uriPattern;
+  }
+
+  private String getConfigPostUri(String uriPattern, String clusterName) {
+    return uriPattern.replace("[PARTICIPANT_CLUSTER]", clusterName);
+  }
+
+  @Override
+  public ClusterSpectator createClusterSpectator(
+      String zkString, String clusterName,
+      String instanceName) {
+    return new ConfigGeneratorClusterSpectatorImpl(zkString, clusterName, instanceName,
+        getConfigPostUri(uriPattern, clusterName));
+  }
+}
