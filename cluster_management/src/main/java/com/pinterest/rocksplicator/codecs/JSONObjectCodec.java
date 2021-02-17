0e6e519d781d25d4a6b6d903b42d50b845070294
Gopal Rajpurohit <grajpurohit@pinterest.com>
2021-02-17 15:43:02 -0800

    Publisher for zk shard_map by resource

--- cluster_management/src/main/java/com/pinterest/rocksplicator/codecs/JSONObjectCodec.java
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/codecs/JSONObjectCodec.java
@@ -2,11 +2,17 @@ package com.pinterest.rocksplicator.codecs;
 
 import org.json.simple.JSONObject;
 
+import java.io.UnsupportedEncodingException;
+
 public class JSONObjectCodec implements Codec<JSONObject, byte[]> {
 
   @Override
   public byte[] encode(JSONObject obj) throws CodecException {
-    return obj.toJSONString().getBytes();
+    try {
+      return obj.toJSONString().getBytes("UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      throw new CodecException(e);
+    }
   }
 
   @Override
