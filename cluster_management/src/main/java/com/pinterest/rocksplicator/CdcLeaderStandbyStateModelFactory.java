05a4d391c28271c627729ce77fc3221673078847
Indy Prentice <indy@pinterest.com>
2021-07-27 22:24:15 +0000

    Add leader/standby state model for CDC

---
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/CdcLeaderStandbyStateModelFactory.java
@@ -0,0 +1,75 @@
+package com.pinterest.rocksplicator;
+
+import org.apache.helix.NotificationContext;
+import org.apache.helix.model.Message;
+import org.apache.helix.participant.AbstractHelixLeaderStandbyStateModel;
+import org.apache.helix.participant.statemachine.StateModel;
+import org.apache.helix.participant.statemachine.StateModelFactory;
+import org.apache.helix.participant.statemachine.StateModelInfo;
+
+
+public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateModel> {
+  public CdcLeaderStandbyStateModelFactory() {
+
+  }
+
+  public static class CdcLeaderStandbyStateModel extends AbstractHelixLeaderStandbyStateModel  {
+    private final String resourceName;
+    private final int adminPort;
+
+    @StateModelInfo(initialState = "OFFLINE", states = {
+        "LEADER", "STANDBY"
+    })
+    public CdcLeaderStandbyStateModel(String resourceName, final String zkConnectString, String partitionName) {
+      super(zkConnectString);
+      super.setPartitionName(partitionName);
+      this.resourceName = resourceName;
+    }
+
+    @Override
+    public void onBecomeLeaderFromStandby(Message message, NotificationContext context) {
+      Utils.checkStateTransitions("STANDBY", "LEADER", message, resourceName, partitionName);
+      Utils.logTransitionMessage(message)
+      // Add observer
+      Utils.logTransitionCompletionMessage(message);
+    }
+
+    @Override
+    public void onBecomeStandbyFromLeader(Message message, NotificationContext context) {
+      Utils.checkStateTransitions("LEADER", "STANDBY", message, resourceName, partitionName);
+      Utils.logTransitionMessage(message);
+        String dbName = Utils.getDbName(this.partitionName);
+      // Remove observer
+      Utils.logTransitionCompletionMessage(message);
+    }
+
+    @Override
+    public void onBecomeStandbyFromOffline(Message message, NotificationContext context) {
+      Utils.checkStateTransitions("OFFLINE", "STANDBY", message, resourceName, partitionName);
+      Utils.logTransitionMessage(message);
+
+      Utils.logTransitionCompletionMessage(message);
+    }
+
+    @Override
+    public void onBecomeOfflineFromStandby(Message message, NotificationContext context) {
+      Utils.checkStateTransitions("STANDBY", "OFFLINE", message, resourceName, partitionName);
+      Utils.logTransitionMessage(message);
+
+      Utils.logTransitionCompletionMessage(message);
+    }
+
+    @Override
+    public void onBecomeDroppedFromOffline(Message message, NotificationContext context) {
+      Utils.checkStateTransitions("OFFLINE", "DROPPED", message, resourceName, partitionName);
+      Utils.logTransitionMessage(message);
+
+      Utils.logTransitionCompletionMessage(message);
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    }
+}
