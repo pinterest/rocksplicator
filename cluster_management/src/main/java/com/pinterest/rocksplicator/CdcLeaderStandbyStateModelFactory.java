5caa148ddcbfb52b8fbd67dac3e8b313182a59e9
Indy Prentice <indy@pinterest.com>
2021-07-29 21:18:46 +0000

    Add comments to state model impl

--- cluster_management/src/main/java/com/pinterest/rocksplicator/CdcLeaderStandbyStateModelFactory.java
+++ cluster_management/src/main/java/com/pinterest/rocksplicator/CdcLeaderStandbyStateModelFactory.java
@@ -1,5 +1,8 @@
 package com.pinterest.rocksplicator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.apache.helix.NotificationContext;
 import org.apache.helix.model.Message;
 import org.apache.helix.participant.AbstractHelixLeaderStandbyStateModel;
@@ -17,12 +20,40 @@ import org.apache.helix.participant.statemachine.StateModelInfo;
  * @StateModelInfo(initialState = "OFFLINE", states = {
  *     "LEADER", "STANDBY"
  * })
+
+ * The LeaderStandby state machine has 5 possible states. There are 8 possible state transitions
+ * that
+ * we need to handle.
+ *                         1           3             5
+ *                LEADER  <-   STANDBY  <-   OFFLINE  ->  DROPPED
+ *                        ->           ->        ^   <-  ^
+ *                         2           4        7 \  6  / 8
+ *                                                 ERROR
+ *
+ * 1) Standby to Leader
+ *    TODO(indy): Add observer
+ *
+ * 2) Leader to Standby
+ *    TODO(indy): Remove observer
+ *
+ * 3) Offline to Standby
+ *
+ * 4) Standby to Offline
+ *
+ * 5) Offline to Dropped
+ *
+ * 6) Dropped to Offline
+ *
+ * 7) Error to Offline
+ *
+ * 8) Error to Dropped
  */
 public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateModel> {
+  private static final Logger LOG = LoggerFactory.getLogger(CdcLeaderStandbyStateModelFactory.class);
   final String zkConnectString;
 
   public CdcLeaderStandbyStateModelFactory(final String zkConnectString) {
-    this.zkConnectString = zkConnectString
+    this.zkConnectString = zkConnectString;
   }
 
   @Override
@@ -37,26 +68,32 @@ public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateMo
 
   public static class CdcLeaderStandbyStateModel extends AbstractHelixLeaderStandbyStateModel  {
     private final String resourceName;
-    private final int adminPort;
+    private final String partitionName;
 
     @StateModelInfo(initialState = "OFFLINE", states = {
         "LEADER", "STANDBY"
     })
     public CdcLeaderStandbyStateModel(String resourceName, final String zkConnectString, String partitionName) {
       super(zkConnectString);
-      super.setPartitionName(partitionName);
+      this.partitionName = partitionName;
       this.resourceName = resourceName;
     }
 
     @Override
+    /**
+     * 1) Standby to Leader
+     */
     public void onBecomeLeaderFromStandby(Message message, NotificationContext context) {
       Utils.checkStateTransitions("STANDBY", "LEADER", message, resourceName, partitionName);
-      Utils.logTransitionMessage(message)
+      Utils.logTransitionMessage(message);
       // Add observer
       Utils.logTransitionCompletionMessage(message);
     }
 
     @Override
+    /**
+     * 2) Leader to Standby
+     */
     public void onBecomeStandbyFromLeader(Message message, NotificationContext context) {
       Utils.checkStateTransitions("LEADER", "STANDBY", message, resourceName, partitionName);
       Utils.logTransitionMessage(message);
@@ -66,6 +103,9 @@ public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateMo
     }
 
     @Override
+    /**
+     * 3) Offline to Standby
+     */
     public void onBecomeStandbyFromOffline(Message message, NotificationContext context) {
       Utils.checkStateTransitions("OFFLINE", "STANDBY", message, resourceName, partitionName);
       Utils.logTransitionMessage(message);
@@ -74,6 +114,9 @@ public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateMo
     }
 
     @Override
+    /**
+     * 4) Standby to Offline
+     */
     public void onBecomeOfflineFromStandby(Message message, NotificationContext context) {
       Utils.checkStateTransitions("STANDBY", "OFFLINE", message, resourceName, partitionName);
       Utils.logTransitionMessage(message);
@@ -82,6 +125,9 @@ public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateMo
     }
 
     @Override
+    /**
+     * 5) Offline to Dropped
+     */
     public void onBecomeDroppedFromOffline(Message message, NotificationContext context) {
       Utils.checkStateTransitions("OFFLINE", "DROPPED", message, resourceName, partitionName);
       Utils.logTransitionMessage(message);
@@ -90,6 +136,9 @@ public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateMo
     }
 
     @Override
+    /**
+     * 6) Dropped to Offline
+     */
     public void onBecomeOfflineFromDropped(Message message, NotificationContext context) {
   	  super.onBecomeOfflineFromDropped(message, context);
       Utils.checkStateTransitions("DROPPED", "OFFLINE", message, resourceName, partitionName);
@@ -99,6 +148,9 @@ public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateMo
     }
   
     @Override
+    /**
+     * 7) Error to Offline
+     */
     public void onBecomeOfflineFromError(Message message, NotificationContext context) {
   	  super.onBecomeOfflineFromError(message, context);
       Utils.checkStateTransitions("ERROR", "OFFLINE", message, resourceName, partitionName);
@@ -106,6 +158,19 @@ public class CdcLeaderStandbyStateModelFactory extends StateModelFactory<StateMo
 
       Utils.logTransitionCompletionMessage(message);
     }
+
+    @Override
+    /**
+     * 8) Error to Dropped
+     */
+    public void onBecomeDroppedFromError(Message message, NotificationContext context) throws Exception {
+  	  super.onBecomeDroppedFromError(message, context);
+      Utils.checkStateTransitions("ERROR", "DROPPED", message, resourceName, partitionName);
+      Utils.logTransitionMessage(message);
+
+      Utils.logTransitionCompletionMessage(message);
+    }
+
 
     @Override
     public void reset() {
