b9d0b51b1a0468a3dc2bc4211852511fbd1eacf4
Indy Prentice <indy@pinterest.com>
2021-09-08 18:50:07 +0000

    in CDC admin, accept db wrapper implementation passed in

--- cdc_admin/cdc_admin_handler.h
+++ cdc_admin/cdc_admin_handler.h
@@ -27,6 +27,7 @@
 #else
 #include "cdc_admin/gen-cpp2/CdcAdmin.h"
 #endif
+#include "rocksdb_replicator/test_db_proxy.h"
 
 namespace cdc_admin {
 
@@ -72,6 +73,12 @@ protected:
   // Put db_admin_lock in protected to provide flexibility
   // of overriding some admin functions
   common::ObjectLock<std::string> db_admin_lock_;
+
+  virtual std::unique_ptr<replicator::DbWrapper> getDbWrapper(std::string db_name) {
+      std::unique_ptr<replicator::TestDBProxy> db_wrapper =
+          std::make_unique<replicator::TestDBProxy>(db_name, 0);
+      return db_wrapper;
+  }
 
 private:
   std::unique_ptr<replicator::DbWrapper> removeDB(const std::string& db_name,
